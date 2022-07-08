497d61adf7c4cb7c070e9f78f7876772df56c62e
charles mitchell <48700633+cmitchell7376@users.noreply.github.com>
2022-07-08 11:19:59 -0500

    status change

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function inti(){
+    let takeoff = document.getElementById("takeoff");
+    let landing = document.getElementById("landing");
+    let missionAbort = document.getElementById("missionAbort");
+    let up = document.getElementById("up");
+    let down = document.getElementById("down");
+    let left = document.getElementById("left");
+    let right = document.getElementById("right");
+    let flightStatus = document.getElementById("flightStatus");
+
+    function statusChange(){
+        flightStatus.innerHTML = "Shuttle in flight.";
+    }
+
+    if(window.confirm("Is the shuttle ready for takeoff!" === true)){
+        takeoff.addEventListener("click",statusChange);
+    }
+
+
+}
+window.addEventListener("load",inti);
